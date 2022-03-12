4e74b61a89f224ad512289382483bd76d61b22b5
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-03-12 22:30:28 +0100

    add nav-bar

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 app.get("/", function (req, res, next) {
   res.send("Hello World!");
 });
+
+app.use('/static', express.static('static'));
 var server = app.listen(3000, function () {
   console.log("Running on :3000");
 });
