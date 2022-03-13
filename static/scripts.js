36e280fb398ebfac2ed03f88875d651bacdbc4db
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-03-13 13:48:57 +0100

    add new pages

---
+++ static/scripts.js
@@ -0,0 +1,20 @@
+window.addEventListener("load",main)
+
+
+function main(){
+    console.log("script loaded")
+
+    const openNavButton = document.getElementById("open-nav-btn")
+    const mobileNav = document.getElementById("mobile-nav")
+    const closeNavButton = document.getElementById("close-nav-btn")
+
+    openNavButton.addEventListener("click",_ => toggleDisplay(mobileNav) )
+    closeNavButton.addEventListener("click",_ => toggleDisplay(mobileNav) )
+
+}
+
+function toggleDisplay(element){
+    element.classList.toggle("hidden")
+}
+
+
