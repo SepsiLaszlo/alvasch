68044eed9ece60d8c2e6a20b407e2b1716294e01
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-03-13 16:18:35 +0100

    add routing

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require("express");
+const path = require("path");
+
 var app = express();
+
+app.use("/static", express.static("static"));
+
 app.get("/", function (req, res, next) {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname + "/static/bed.html"));
+});
+app.get("/bed", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/static/bed.html"));
 });
 
-app.use('/static', express.static('static'));
+app.get("/user", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/static/user.html"));
+});
+
+app.get("/reservation", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/static/reservation.html"));
+});
+
+app.get("/reservation/:id", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/static/reservation-1.html"));
+});
+
 var server = app.listen(3000, function () {
   console.log("Running on :3000");
 });
