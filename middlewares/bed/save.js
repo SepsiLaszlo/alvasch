89f57e0a03d99aacf1bf8f211910f7fc06845da8
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-04-24 10:04:19 +0200

    update views

--- middlewares/bed/save.js
+++ middlewares/bed/save.js
@@ -11,24 +11,15 @@
      const Bed = requireOption(objectrepository, 'Bed');
  
      return function(req, res, next) {
-         console.log('bed save')
-         
         if (
              typeof req.body.room === 'undefined' ||
-             typeof req.body.position === 'undefined'||
-             typeof req.body.owner === 'undefined'
+             typeof req.body.position === 'undefined'
          ) {
              return next();
          }
  
- 
-         if (Number.isNaN(parseInt(req.body.room, 10))) {
-             return next(new Error('A szobaszámak számnak kell lennie! (bruh)'));
-         }
- 
          res.locals.bed.room = req.body.room;
          res.locals.bed.position = req.body.position; 
-         res.locals.bed.user.name = req.body.owner;
         
  
          res.locals.bed.save(err => {
@@ -36,13 +27,7 @@
                  return next(err);
              }
 
-             res.locals.bed.user.save(err => {
-                if (err) {
-                    return next(err);
-                }
- 
              return res.redirect(`/bed/${res.locals.bed._id}`);
-            });
          });
      };
  };
